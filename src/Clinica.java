03889aaf936df7662af77445e4c596e5cae86be9
GitdoPedro <emailpedrolima@gmail.com>
2023-02-14 18:04:12 -0300

    [M2S03] Ex 2 - Cálculo do IMC

--- src/Clinica.java
+++ src/Clinica.java
@@ -5,5 +5,20 @@ public class Clinica {
     String endereco;
     int codigoClinica;
 
+    float imc(float peso,float altura){
+        float imcResultado = peso/(altura*altura);
+        if (imcResultado <= 18.5){
+            System.out.println("Magreza");
+        }else if (imcResultado <= 24.9){
+            System.out.println("Normal");
+        }else if (imcResultado <= 29.9){
+            System.out.println("Sobrepeso");
+        }else if (imcResultado <= 39.9){
+            System.out.println("Obesidade");
+        }else{
+            System.out.println("Obesidade grave");
+        }
+        return imcResultado;
+    }
 
 }
